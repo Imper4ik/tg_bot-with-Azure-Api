876090931a04a9281f902a31357cecee0244296c
Butouski Mikita <nindse@gmail.com>
2024-12-03 15:57:34 +0100

    delete unnessasary print in Text_to_Speech

--- app/handlers.py
+++ app/handlers.py
@@ -219,19 +219,12 @@ async def request_text_for_speech(message: types.Message, state: FSMContext):
     # Запрашиваем у пользователя текст для озвучивания
     await message.reply("Введите текст, который вы хотите озвучить. (en)")
 
-    # Логируем текущие состояния для отладки
-    current_state = await state.get_state()
-    print(f"Текущее состояние: {current_state}")
-
     # Переходим в состояние ожидания текста для озвучки
     await state.set_state(SpeechStates.waiting_for_speech_text)
 
 
 @router.message(SpeechStates.waiting_for_speech_text)
 async def handle_text_to_speech(message: types.Message, state: FSMContext):
-    # Логируем текущее состояние
-    current_state = await state.get_state()
-    print(f"Текущее состояние перед обработкой текста: {current_state}")
 
     text_to_speech = message.text
 
