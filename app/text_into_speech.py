68ec3fb9e163689303449812d703174349abdcd3
Butouski Mikita <nindse@gmail.com>
2024-12-05 18:06:43 +0100

    update text_into_speech

--- app/text_into_speech.py
+++ app/text_into_speech.py
@@ -1,11 +1,18 @@
 import logging
-import os
 from boto3 import Session
 from botocore.exceptions import BotoCoreError, ClientError
 
 
 class TextToSpeech:
-    def __init__(self, aws_access_key: str, aws_secret_key: str, aws_region_name: str, voice_id: str = "Joanna"):
+    # Словарь голосов для поддерживаемых языков
+    VOICE_MAP = {
+        'en': {'male': 'Matthew', 'female': 'Joanna'},
+        'ru': {'male': 'Maxim', 'female': 'Tatyana'},
+        'pl': {'male': 'Mariusz', 'female': 'Ewa'},
+        'de': {'male': 'Hans', 'female': 'Marlene'}
+    }
+
+    def __init__(self, aws_access_key: str, aws_secret_key: str, aws_region_name: str):
         """
         Инициализация клиента AWS Polly.
         """
@@ -15,20 +22,26 @@ class TextToSpeech:
             region_name=aws_region_name
         )
         self.polly = self.session.client("polly")
-        self.voice_id = voice_id
 
-    def synthesize_speech(self, text: str, output_file: str = "speech.mp3") -> str:
+    def synthesize_speech(self, text: str, language: str, gender: str, output_file: str = "speech.mp3") -> str:
         """
         Генерация речи из текста и сохранение в аудиофайл.
         :param text: Текст для озвучивания.
+        :param language: Код языка (например, 'en', 'ru').
+        :param gender: Пол ('male' или 'female').
         :param output_file: Путь для сохранения MP3-файла.
         :return: Путь к сгенерированному файлу.
         """
+        if language not in self.VOICE_MAP or gender not in self.VOICE_MAP[language]:
+            raise ValueError(f"Неподдерживаемый язык ({language}) или пол ({gender}).")
+
+        voice_id = self.VOICE_MAP[language][gender]
+
         try:
             response = self.polly.synthesize_speech(
                 Text=text,
                 OutputFormat="mp3",
-                VoiceId=self.voice_id
+                VoiceId=voice_id
             )
 
             with open(output_file, "wb") as file:
